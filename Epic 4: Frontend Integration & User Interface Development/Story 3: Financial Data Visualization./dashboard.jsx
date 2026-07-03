3fcc0975bd06106f3a56636285bc4b6766be0bc6
Syed Zainul Abdin <zaintech14@gmail.com>
2026-07-04 01:53:21 +0530

    Create dashboard.jsx

---
+++ Epic 4: Frontend Integration & User Interface Development/Story 3: Financial Data Visualization./dashboard.jsx
@@ -0,0 +1,17 @@
+function Dashboard() {
+  return (
+    <div style={{ color: "white", padding: "30px" }}>
+      <h1>Dashboard Overview</h1>
+
+      <h3>Monthly Income: ₹60,000</h3>
+
+      <h3>Monthly Expenses: ₹25,000</h3>
+
+      <h3>Monthly Surplus: ₹35,000</h3>
+
+      <h3>Stress Level: Low</h3>
+    </div>
+  );
+}
+
+export default Dashboard;
